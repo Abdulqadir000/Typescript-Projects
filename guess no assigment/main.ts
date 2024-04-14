18fa3e0df10cee37e3049ed62268c26b0cd74ae7
Abdulqadir000 <154420547+Abdulqadir000@users.noreply.github.com>
2024-04-14 16:32:01 +0500

    m

---
+++ guess no assigment/main.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+const sysgenno = Math.floor(Math.random());
+
+const answer: {
+  system: number;
+} = await inquirer.prompt({
+  type: "number",
+  name: "system",
+  message: "Guess Number:",
+});
+
+const { system } = answer;
+if (system === sysgenno) {
+  console.log("Congratulation your number is correct");
+} else {
+  console.log("try again");
+}
